0875480cd25f2f90b047b3c615abeea0706ff6ae
Mariana V <marianvj17@gmail.com>
2022-08-01 17:28:50 -0500

    git commit -m "modiificacion"

---
+++ assets/js/variables.js
@@ -0,0 +1,63 @@
+// Mejores prácticas
+
+// camelCase
+//Utilizar mayusculas en la mitad de nuestras variables
+//variablesTriangulo, miNombre, miEdad, miDireccion 
+
+//Palabras reservadas
+//No usar palabras reservadas para declarar una variable
+// if, return, true, false, for, while, let, var, etc...
+
+//var while;
+//var null ;
+//var if;
+//var true;
+//var false;
+//var var;
+//var const;
+//var let;
+
+//Seguir un estandar personal
+
+//Pueden contener nuemeros, letras, guion bajo, simbolo de dolar
+var a;
+var a1;
+var a_Nombre;
+var a$Dirección;
+
+
+//Declaracion de variables
+//Type Numero
+var edad = 27;
+var peso = 60;
+var astigmatismo = 2;
+
+//Type Boolean
+var ciega = true;
+var hambre = true;
+var bateria = false; 
+
+//Type String
+var nombre = "Mariana Valladolid" ;
+var ciudad = "Guadalajara";
+var nacionalidad = "Mexicana";
+
+
+//Type null
+var ganasDeBibir = null ;
+var nineros = null; 
+
+//Variables declaradas con un mismo valor
+var x, y, z = 4
+
+//Variables undefined
+var xx, yy, zz;
+
+// Var tres= 4, 
+//Var dos = tres
+//Var uno = tres
+var uno = dos = tres = 4;
+
+//Tarea escribir en una sola liena variables undefined en nuestro carrito de compras
+
+//var huevos, arroz para sushi, pure de tomate, pinol, servilletas, pasta de dientes, 
