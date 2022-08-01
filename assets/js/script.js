787fd8e9d7ff99dbd9c005f851c5fca02ee113dc
Mariana V <marianvj17@gmail.com>
2022-08-01 13:24:00 -0500

    Semana 5 Introduccion a  JS

---
+++ assets/js/script.js
@@ -0,0 +1,75 @@
+
+
+//Este es un comentario de una sola linea usando dos barras o diagonales
+
+
+/*Este es un comentario multilinea usando una diagonal y asterisco para abrir
+y asterisco diagonal para cerrar*/
+
+/* */     /* Este es un comentario de una sola linea usando
+una diagonal y asterisco para abrir
+y asterisco diagonal para cerrar*/
+
+//Una alerta es una ventana de dialogo que muestra un mensaje 
+//alert("Buen día a todes");
+
+
+//Pedimos informacion de ususario con un prompt, la guardamos en una variable llamada nombre para despues mostrarla
+nombre a prompt ("¿Cómo te llamas?");
+
+//Usamos una console.log para mostar lo que se guarda en la variable llamada nombre 
+console.log(nombre); 
+
+//Java Script en case sensitive, por lo tanto si escribimos "Hola" y "hola" tendremos dos cosas completamente diferentes.
+
+//UpperCamelCase - ComidaFavoritaDeFelipe 
+//lowerCamelCase  - comidaFavoritaDeFelipe
+
+//nombre = "Juan";
+//nombre = "juan"
+
+//Comida-Favorita-de-Marina 
+
+//comidaFavoritaDeMariana
+
+
+
+
+//Programa para calcular el area de un triangulo
+
+//Definimos 2 variables 
+base= prompt("Ingrese la base del triangulo");
+altura=("Ingrese la altura del triangulo")
+
+//Creamos una formula que utilice los datos ingresados
+areaTriangulo = (base * alturs)/2
+
+
+alert("areaTriangulo");
+
+
+//Concatenacion de mensaje + resultado
+console.log("El area triangulo" + areaTriangulo);
+
+
+ojos
+oidos
+piernas
+manos 
+brazos 
+piecitos 
+pulmones
+
+
+
+function respira
+function dormir
+function comentario
+function prepararTaquitos
+
+
+
+
+
+
+//CamelClase 
